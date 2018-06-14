099a6a1ece07dd025dfccfca25d3a9af0eb0d5f5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-14 10:06:08 -0700

    declared the global vars

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// GLOABAL VARS
+const form              = document.querySelector('#loan-form');
+const loanAmount        = document.querySelector('#amount');
+const interest          = document.querySelector('#interest');
+const years             = document.querySelector('#years');
+const monthlyPayment    = document.querySelector('#monthly-payment');
+const totalPayment      = document.querySelector('#total-payment');
+const totalInterest     = document.querySelector('#total-interest');
