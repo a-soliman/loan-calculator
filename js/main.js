3656f09cace32ca46c54a3e297d009f7359d7368
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-14 10:07:05 -0700

    coded the main calculate function

--- js/main.js
+++ js/main.js
@@ -10,3 +10,26 @@ const totalInterest     = document.querySelector('#total-interest');
 // LISTEN FOR SUBMIT
 form.addEventListener('submit', claculate);
 
+// CALCULATE LOAN
+function claculate(e) {
+    e.preventDefault();
+
+    const values = getValues();
+    
+    if (!validateValues(values)) {
+        return raiseError("Check your numbers!");
+    }
+    
+    // COMPUTE MONTHLY PAYMENTS
+    const principal             = values.amount;
+    const calculatedInterest    = values.interest / 100 / 12;
+    const calculatedPayments    = values.years * 12;
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    monthlyPayment.value    = monthly.toFixed(2);
+    totalPayment.value      = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value     = ((monthly * calculatedPayments) - principal).toFixed(2);
+    return;
+}
